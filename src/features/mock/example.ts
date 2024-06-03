ed5a9dc93c167eda0aa3e55510bad208e313cba7
Ofir <ofir.zeitoun@gmail.com>
2024-06-04 00:48:09 +0300

    exercises

--- src/features/mock/example.ts
+++ src/features/mock/example.ts
@@ -1,3 +1,13 @@
 export function add(a: number, b: number) {
   return a + b;
 }
+
+export function subtract(a: number, b: number) {
+  return a - b;
+}
+
+export async function asyncAdd(a: number, b: number) {
+  return a + b;
+}
+
+export const increment = (value: number): number => value + 1;
--- src/features/mock/mocks.test.ts
+++ src/features/mock/mocks.test.ts
@@ -1,5 +1,5 @@
 import { jest } from "@jest/globals";
-import { add } from "./example";
+import { add, subtract, asyncAdd, increment } from "./example";
 
 jest.mock("./example", () => {
   const originalModule = jest.requireActual("./example");
@@ -16,5 +16,32 @@ jest.mock("./example", () => {
 describe("mock module example", () => {
   it("should not return 3", () => {
     expect(add(1, 2)).toBe(2);
+  });
+
+  it("subtracts two numbers", () => {
+    const subtractMock = jest.fn(subtract);
+    subtractMock(4, 3);
+    expect(subtractMock).toHaveBeenCalledWith(4, 3);
+    expect(subtractMock).toHaveReturnedWith(1);
+  });
+
+  it("should return 3 async", (done) => {
+    const mockAsyncAdd = jest.fn(asyncAdd).mockResolvedValue(3);
+    (async () => {
+      expect(await mockAsyncAdd(10, 5)).toBe(3);
+      done();
+    })();
+  });
+
+  it("increment gets called multiple times", () => {
+    const incrementMock = jest.fn(increment);
+    incrementMock(1);
+    incrementMock(2);
+    incrementMock(3);
+
+    expect(incrementMock).toHaveBeenCalledTimes(3);
+    expect(incrementMock).toHaveBeenCalledWith(1);
+    expect(incrementMock).toHaveBeenCalledWith(2);
+    expect(incrementMock).toHaveBeenCalledWith(3);
   });
 });
