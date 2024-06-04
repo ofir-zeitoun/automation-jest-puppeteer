0565d3cc61573676c84935dfe73b7885623f1d89
Ofir <ofir.zeitoun@gmail.com>
2024-06-04 21:26:56 +0300

    reuse when

--- src/features/calculator/calculator.feature
+++ src/features/calculator/calculator.feature
@@ -3,5 +3,18 @@ Feature: Calculator
   Scenario: Add two numbers
 
     Given first number is 4
-    When I add 3
-    Then I should get 7
+    When second number is 3
+    Then Their sum should be 7
+
+  Scenario: Add another two numbers
+
+    Given first number is 10
+    When second number is 20
+    Then Their sum should be 30
+
+    
+  Scenario: Subtract two numbers
+
+    Given first number is 30
+    When second number is 10
+    Then Their diff should be 20
--- src/features/calculator/calculator.steps.ts
+++ src/features/calculator/calculator.steps.ts
@@ -2,19 +2,25 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
 //@ts-ignore
 const assert = require("assert");
-const { add } = require("./calculator");
+const { add, subtract } = require("./calculator");
 
 let first: number;
-let sum: number;
+let second: number;
 
 Given("first number is {int}", function (firstNumber: number) {
   first = firstNumber;
 });
 
-When("I add {int}", function (secondNumber: number) {
-  sum = add(first, secondNumber);
+When("second number is {int}", function (secondNumber: number) {
+  second = secondNumber;
 });
 
-Then("I should get {int}", function (expectedResult: number) {
+Then("Their sum should be {int}", function (expectedResult: number) {
+  const sum = add(first, second);
+  assert.equal(sum, expectedResult);
+});
+
+Then("Their diff should be {int}", function (expectedResult: number) {
+  const sum = subtract(first, second);
   assert.equal(sum, expectedResult);
 });
