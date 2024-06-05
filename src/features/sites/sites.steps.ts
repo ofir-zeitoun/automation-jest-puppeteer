0f15e3313f37684207993ef1a1cfd8fe32614551
Ofir <ofir.zeitoun@gmail.com>
2024-06-05 21:30:56 +0300

    arrow functions to functions

--- src/features/sites/sites.steps.ts
+++ src/features/sites/sites.steps.ts
@@ -24,7 +24,7 @@ let browser: Browser;
 let page: Page;
 let postData: Record<string, string>;
 
-BeforeAll(async () => {
+BeforeAll(async function () {
   browser = await puppeteer.launch({
     headless: env.headless,
     slowMo: env.slowMo,
@@ -32,15 +32,15 @@ BeforeAll(async () => {
   });
 });
 
-AfterAll(async () => {
+AfterAll(async function () {
   await browser.close();
 });
 
-After(async () => {
+After(async function () {
   page?.isClosed() || (await page?.close());
 });
 
-Given("I open a site {string}", async (site: string) => {
+Given("I open a site {string}", async function (site: string) {
   page = await browser.newPage();
   await page.goto(site);
   await page.setViewport(env.viewport);
@@ -48,23 +48,23 @@ Given("I open a site {string}", async (site: string) => {
 
 When(
   "I type {string} into selector {string}",
-  async (text: string, selector: string) => {
+  async function (text: string, selector: string) {
     await page.type(selector, text, { delay: 100 });
   }
 );
 
-When("I click on selector {string}", async (selector: string) => {
+When("I click on selector {string}", async function (selector: string) {
   await page.click(selector);
 });
 
-When("I wait for response", () => {
-  page.on("request", async (request) => {
+When("I wait for response", function () {
+  page.on("request", async function (request) {
     const postDataStr = request.postData();
     postData = parsePostData(postDataStr || "");
   });
 });
 
-When("I submit a form on {string}", async (selector: string) => {
+When("I submit a form on {string}", async function (selector: string) {
   const [response] = await Promise.all([
     page.waitForNavigation(),
     page.click(selector),
@@ -75,12 +75,12 @@ When("I submit a form on {string}", async (selector: string) => {
 
 Then(
   "data should have {string} with {string}",
-  async (key: string, value: string) => {
+  async function (key: string, value: string) {
     assert.equal(postData?.[key], value);
   }
 );
 
-Then("The element {string} should exist", async (selector: string) => {
+Then("The element {string} should exist", async function (selector: string) {
   const found = await page.waitForSelector(selector);
   assert.ok(found);
 });
